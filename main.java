fc7fee7a94882f2d238ec893b67d923cc0a31ecc
Caio César Barbosa <167909208+caiocesr@users.noreply.github.com>
2025-10-27 12:40:26 -0300

    Teste refatorado para melhor funcionamento
    Atualização no teste para melhor entendimento do funcionamento de adicionarMemento (salvar o texto) e restaurar (voltar ao texto anteriormente salvo).

--- main.java
+++ main.java
@@ -3,20 +3,28 @@ public class Main{
         EditorTexto editor = new EditorTexto();
         Historico historico = new Historico();
 
-        editor.adicionarTexto("Olá, ");
+        editor.adicionarTexto("Olá ");
         historico.adicionarMemento(editor.adicionarMemento());
 
-        editor.adicionarTexto("Caio");
+        editor.adicionarTexto("Mundo");
         historico.adicionarMemento(editor.adicionarMemento());
 
         editor.adicionarTexto("!");
         historico.adicionarMemento(editor.adicionarMemento());
 
+        System.out.println("O texto final é: ");
+                editor.mostrarTexto();
+
+        editor.restaurar(historico.desfazer());
+        System.out.println("Após desfazer o texto 1:");
         editor.mostrarTexto();
 
         editor.restaurar(historico.desfazer());
+        System.out.println("Após desfazer o texto 2:");
         editor.mostrarTexto();
 
-
+        editor.restaurar(historico.desfazer());
+        System.out.println("Após desfazer 3, o texto final fica:");
+        editor.mostrarTexto();
     }
 }
